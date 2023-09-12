06110460996f93f14e034c40bd6caa168cc544e2
Dávid Komlósi <komlosidavid19990410@gmail.com>
2023-09-12 15:49:01 +0200

    Initial commit

---
+++ src/main/java/inf/unideb/hu/exam/system/ExamSystemApplication.java
@@ -0,0 +1,13 @@
+package inf.unideb.hu.exam.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inf/unideb/hu/exam/system/ExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package inf.unideb.hu.exam.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
