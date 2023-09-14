aa8c3ea3a891505a46e7d28b99cb29a1eab4148f
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-14 17:11:27 +0200

    Update ExamSystemApplication.java

--- src/main/java/inf/unideb/hu/exam/system/ExamSystemApplication.java
+++ src/main/java/inf/unideb/hu/exam/system/ExamSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamSystemApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(ExamSystemApplication.class, args);
 	}
 
