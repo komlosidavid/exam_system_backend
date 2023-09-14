9fa2043ec4100356af769c8bf8648c40c04234f9
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-14 17:11:56 +0200

    Create QuestionType.java

---
+++ src/main/java/inf/unideb/hu/exam/system/models/enums/QuestionType.java
@@ -0,0 +1,20 @@
+
+package inf.unideb.hu.exam.system.models.enums;
+
+/**
+ * Enumerated class for holding question types.
+ */
+public enum QuestionType {
+    /**
+     * If the question has only one option to answer.
+     */
+    ONE_ANSWER,
+    /**
+     * If the question has multiple answers.
+     */
+    MULTIPLE_ANSWER,
+    /**
+     * Questions which are requires explanation.
+     */
+    EXPLANATORY_ANSWER
+}
