8bbd8507cf1cab6cf3afe1cc6c7364766b025227
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 22:51:37 +0200

    Update TestDao.java

--- src/main/java/inf/unideb/hu/exam/system/dao/TestDao.java
+++ src/main/java/inf/unideb/hu/exam/system/dao/TestDao.java
@@ -2,9 +2,12 @@
 package inf.unideb.hu.exam.system.dao;
 
 import inf.unideb.hu.exam.system.models.Test;
+import inf.unideb.hu.exam.system.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -15,5 +18,8 @@ import java.util.UUID;
  */
 @Repository
 public interface TestDao extends JpaRepository<Test, UUID> {
-    Page<Test> findAll(Pageable pageable);
+
+    Page<Test> findByCreatorOrCollaborators(User creator,
+                                            User collaborator,
+                                            Pageable pageable);
 }
