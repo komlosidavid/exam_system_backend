0eb4719687b3128666a29546675355403d026b21
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:22:37 +0200

    Update Configurations.java
    Implemented necessary configurations for security.

--- src/main/java/inf/unideb/hu/exam/system/config/Configurations.java
+++ src/main/java/inf/unideb/hu/exam/system/config/Configurations.java
@@ -1,22 +1,57 @@
 
 package inf.unideb.hu.exam.system.config;
 
+import inf.unideb.hu.exam.system.dao.UserDao;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Configuration class for configure.
  */
 @Configuration
+@RequiredArgsConstructor
 public class Configurations {
 
-    /**
-     * Model mapper bean.
-     * @return a new {@link ModelMapper}.
-     */
+    private final UserDao repository;
+
     @Bean
-    public ModelMapper modelMapper() {
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User was not found!"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
