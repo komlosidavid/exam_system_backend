75da466f81ff2008f675b9eb652534376a533dfc
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:24:05 +0200

    Update CreateUserEntityRequest.java

--- src/main/java/inf/unideb/hu/exam/system/request/CreateUserEntityRequest.java
+++ src/main/java/inf/unideb/hu/exam/system/request/CreateUserEntityRequest.java
@@ -1,6 +1,7 @@
 
 package inf.unideb.hu.exam.system.request;
 
+import inf.unideb.hu.exam.system.models.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,4 +19,8 @@ public class CreateUserEntityRequest {
      * Last name of the user.
      */
     private String lastname;
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
 }
