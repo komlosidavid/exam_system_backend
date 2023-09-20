c906a9dabf59d1857ab21433f88dd5fb150e9063
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 11:24:01 +0200

    Update User.java
    Implemented userdetails properties and functions.

--- src/main/java/inf/unideb/hu/exam/system/models/User.java
+++ src/main/java/inf/unideb/hu/exam/system/models/User.java
@@ -3,16 +3,18 @@ package inf.unideb.hu.exam.system.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import inf.unideb.hu.exam.system.models.enums.Role;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.UUID;
+import java.time.Instant;
+import java.util.*;
 
 /**
  * Entity class for holding user infos.
@@ -23,7 +25,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Data
 @Builder
-public class User {
+public class User implements UserDetails {
 
     /**
      * UUID based primary key.
@@ -35,12 +37,27 @@ public class User {
      * First name of the user.
      */
     @JsonIgnore
+    @Column(nullable = false)
     private String firstname;
     /**
      * Last name of the user.
      */
     @JsonIgnore
+    @Column(nullable = false)
     private String lastname;
+    @Column(nullable = false, unique = true, updatable = false, length = 50)
+    private String username;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(nullable = false)
+    private String password;
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+    @Builder.Default
+    private Instant registeredAt = new Date().toInstant();
 
     /**
      * Function to get the full name of the user.
@@ -49,5 +66,30 @@ public class User {
     @JsonProperty
     public String getFullname() {
         return lastname + " " + firstname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
