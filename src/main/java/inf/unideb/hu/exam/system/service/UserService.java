b53c12cb709b82aa9488af442beb5cee3204cce7
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-20 22:52:59 +0200

    Update UserService.java

--- src/main/java/inf/unideb/hu/exam/system/service/UserService.java
+++ src/main/java/inf/unideb/hu/exam/system/service/UserService.java
@@ -1,21 +1,14 @@
 
 package inf.unideb.hu.exam.system.service;
 
-import inf.unideb.hu.exam.system.models.Pair;
 import inf.unideb.hu.exam.system.models.User;
-import inf.unideb.hu.exam.system.request.CreateUserEntityRequest;
-
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Interface for handling user service methods and functions.
  */
 public interface UserService {
 
-    /**
-     * Function to create a new {@link User} entity.
-     * @param request for creating {@link User} entity.
-     * @return a {@link Pair} class holding the data and the response message.
-     */
-    Pair<Optional<User>> createNewUserEntity(CreateUserEntityRequest request);
+    Page<User> getAllUsers(Pageable pageable);
 }
