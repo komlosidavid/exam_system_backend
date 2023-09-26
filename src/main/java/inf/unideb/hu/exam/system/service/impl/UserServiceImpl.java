a424e3969a16bc9d035d437ec0ecab027916f827
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:32:33 +0200

    Update UserServiceImpl.java
    Created getUserById function.

--- src/main/java/inf/unideb/hu/exam/system/service/impl/UserServiceImpl.java
+++ src/main/java/inf/unideb/hu/exam/system/service/impl/UserServiceImpl.java
@@ -2,12 +2,17 @@
 package inf.unideb.hu.exam.system.service.impl;
 
 import inf.unideb.hu.exam.system.dao.UserDao;
+import inf.unideb.hu.exam.system.models.Pair;
 import inf.unideb.hu.exam.system.models.User;
 import inf.unideb.hu.exam.system.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Class for implementing service methods.
@@ -24,6 +29,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public Page<User> getAllUsers(Pageable pageable) {
         return repository.findAll(pageable);
+    }
+
+    @Override
+    public Pair<Optional<User>> getUserById(UUID id) {
+        Optional<User> userOptional = repository.findById(id);
+
+        if (userOptional.isPresent()) {
+            return new Pair<>(userOptional, null);
+        }
+
+        return new Pair<>(Optional.empty(), "User was not found!");
     }
 
 }
