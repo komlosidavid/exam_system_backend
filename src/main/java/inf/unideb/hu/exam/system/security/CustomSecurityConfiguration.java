cc394ff44883f6b16d16075deff3d51c90c24301
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:31:43 +0200

    Update CustomSecurityConfiguration.java

--- src/main/java/inf/unideb/hu/exam/system/security/CustomSecurityConfiguration.java
+++ src/main/java/inf/unideb/hu/exam/system/security/CustomSecurityConfiguration.java
@@ -2,12 +2,14 @@ package inf.unideb.hu.exam.system.security;
 
 import inf.unideb.hu.exam.system.security.token.TokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
