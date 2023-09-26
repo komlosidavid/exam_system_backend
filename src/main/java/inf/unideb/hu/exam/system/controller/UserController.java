da876086e27c87776f41eadbcff752d9d5263bca
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:30:55 +0200

    Update UserController.java
    Created getUser controller method.

--- src/main/java/inf/unideb/hu/exam/system/controller/UserController.java
+++ src/main/java/inf/unideb/hu/exam/system/controller/UserController.java
@@ -1,14 +1,24 @@
 package inf.unideb.hu.exam.system.controller;
 
 import inf.unideb.hu.exam.system.dto.UserDto;
+import inf.unideb.hu.exam.system.models.Pair;
+import inf.unideb.hu.exam.system.models.ResponseMessage;
+import inf.unideb.hu.exam.system.models.User;
 import inf.unideb.hu.exam.system.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -29,5 +39,24 @@ public class UserController {
         return result.map(user ->
                 modelMapper.map(user, UserDto.class));
     }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = APPLICATION_JSON_VALUE,
+            path = "{id}"
+    )
+    public ResponseEntity<?> getUser(@PathVariable UUID id) {
+        Pair<Optional<User>> serviceResponse = service.getUserById(id);
+
+        if (serviceResponse.getValue().isPresent()) {
+            return new ResponseEntity<>(modelMapper
+                    .map(serviceResponse.getValue().get(),
+                            UserDto.class), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(
+                new ResponseMessage(serviceResponse.getMessage()),
+                HttpStatus.NOT_FOUND);
+     }
 
 }
