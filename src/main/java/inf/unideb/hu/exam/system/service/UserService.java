7009337c8a1f0d9c26536de8f8cf3102587e7a0a
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-09-26 23:32:37 +0200

    Update UserService.java

--- src/main/java/inf/unideb/hu/exam/system/service/UserService.java
+++ src/main/java/inf/unideb/hu/exam/system/service/UserService.java
@@ -1,9 +1,13 @@
 
 package inf.unideb.hu.exam.system.service;
 
+import inf.unideb.hu.exam.system.models.Pair;
 import inf.unideb.hu.exam.system.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Interface for handling user service methods and functions.
@@ -11,4 +15,6 @@ import org.springframework.data.domain.Pageable;
 public interface UserService {
 
     Page<User> getAllUsers(Pageable pageable);
+
+    Pair<Optional<User>> getUserById(UUID id);
 }
