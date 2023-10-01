ce5521b90cf65713dc2b7f871de6bd6c3300df15
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-01 13:43:12 +0200

    Update UserDao.java
    Implemented function to get users by role.

--- src/main/java/inf/unideb/hu/exam/system/dao/UserDao.java
+++ src/main/java/inf/unideb/hu/exam/system/dao/UserDao.java
@@ -2,6 +2,7 @@
 package inf.unideb.hu.exam.system.dao;
 
 import inf.unideb.hu.exam.system.models.User;
+import inf.unideb.hu.exam.system.models.enums.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,4 +25,5 @@ public interface UserDao extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(@Param("username") String username);
     Optional<User> findByEmail(@Param("email") String email);
     Page<User> findAll(Pageable pageable);
+    Page<User> findAllByRole(@Param("role") Role role, Pageable pageable);
 }
