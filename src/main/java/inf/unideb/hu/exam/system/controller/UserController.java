a91a9fee5994327efea3c5f1afea3d426f16c918
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-06 13:25:28 +0200

    Update UserController.java
    Reimplemented user controller to be more reusable.

--- src/main/java/inf/unideb/hu/exam/system/controller/UserController.java
+++ src/main/java/inf/unideb/hu/exam/system/controller/UserController.java
@@ -4,18 +4,15 @@ import inf.unideb.hu.exam.system.dto.UserDto;
 import inf.unideb.hu.exam.system.models.Pair;
 import inf.unideb.hu.exam.system.models.ResponseMessage;
 import inf.unideb.hu.exam.system.models.User;
+import inf.unideb.hu.exam.system.models.enums.Role;
 import inf.unideb.hu.exam.system.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -43,30 +40,30 @@ public class UserController {
     @RequestMapping(
             method = RequestMethod.GET,
             produces = APPLICATION_JSON_VALUE,
-            path = "{id}"
+            path = "/get"
     )
-    public ResponseEntity<?> getUser(@PathVariable UUID id) {
-        Pair<Optional<User>> serviceResponse = service.getUserById(id);
+    public ResponseEntity<?> getUser(@RequestParam(name = "id", required = false) UUID id,
+                                     @RequestParam(name = "role", required = false) String role,
+                                     Pageable pageable) {
+        if (id != null) {
+            Pair<Optional<User>> serviceResponse = service.getUserById(id);
 
-        if (serviceResponse.getValue().isPresent()) {
-            return new ResponseEntity<>(modelMapper
-                    .map(serviceResponse.getValue().get(),
-                            UserDto.class), HttpStatus.OK);
+            if (serviceResponse.getValue().isPresent()) {
+                return new ResponseEntity<>(modelMapper
+                        .map(serviceResponse.getValue().get(),
+                                UserDto.class), HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(
+                    new ResponseMessage(serviceResponse.getMessage()),
+                    HttpStatus.NOT_FOUND);
+        } else if (role != null) {
+            var result = service.getAllUsersByRole(Role.valueOf(role), pageable);
+            return new ResponseEntity<>(
+                    result.map(user -> modelMapper.map(user, UserDto.class)),
+                    HttpStatus.OK);
+        } else {
+            return ResponseEntity.badRequest().build();
         }
-
-        return new ResponseEntity<>(
-                new ResponseMessage(serviceResponse.getMessage()),
-                HttpStatus.NOT_FOUND);
     }
-
-    @RequestMapping(
-            method = RequestMethod.GET,
-            produces = APPLICATION_JSON_VALUE,
-            path = "/teachers"
-    )
-    public Page<UserDto> getAllTeachers(Pageable pageable) {
-        var result = service.getAllTeachers(pageable);
-        return result.map(user -> modelMapper.map(user, UserDto.class));
-    }
-
 }
