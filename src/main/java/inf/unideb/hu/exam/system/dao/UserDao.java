a688820be3a50bfa04a534a2c5798505b2d946ea
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-07 20:33:27 +0200

    Update UserDao.java
    Implemented sql query to get the users by containing full name.

--- src/main/java/inf/unideb/hu/exam/system/dao/UserDao.java
+++ src/main/java/inf/unideb/hu/exam/system/dao/UserDao.java
@@ -6,12 +6,12 @@ import inf.unideb.hu.exam.system.models.enums.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 /**
  * DAO interface for implementing database manipulation
@@ -48,4 +48,17 @@ public interface UserDao extends JpaRepository<User, UUID> {
      * @return a {@link Page} of the result {@link User} entities.
      */
     Page<User> findAllByRole(@Param("role") Role role, Pageable pageable);
+
+    /**
+     * Function to find all the {@link User} entities by full name.
+     * @param name of the {@link User}.
+     * @return a {@link List} of {@link User} entities.
+     */
+    @Query("""
+        SELECT u 
+            FROM User u 
+            WHERE LOWER(u.firstname) LIKE %:name% OR 
+            LOWER(u.lastname) LIKE %:name%
+    """)
+    List<User> findAllByFullNameContains(String name);
 }
