41db7496d3922a4ad0a59e517ba5a316c370347a
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-09 22:12:41 +0200

    Update TestDao.java
    Implemented sql query for subject containing (not working properly.)

--- src/main/java/inf/unideb/hu/exam/system/dao/TestDao.java
+++ src/main/java/inf/unideb/hu/exam/system/dao/TestDao.java
@@ -6,7 +6,11 @@ import inf.unideb.hu.exam.system.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -42,4 +46,24 @@ public interface TestDao extends JpaRepository<Test, UUID> {
      * @return a {@link Page} object of the {@link Test} results.
      */
     Page<Test> findByCollaborators(User user, Pageable pageable);
+
+    /**
+     * Function to get all {@link Test} entities by subject property.
+     * @param subject of the {@link Test}.
+     * @param username for getting the {@link Test} entities which has access by the {@link User}.
+     * @return a {@link List} of {@link Test} entities.
+     */
+    @Query("""
+        SELECT t
+            FROM Test t
+            inner join User creator on t.creator.id = creator.id
+            left join User collaborator on t.id = collaborator.id
+            left join User student on t.id = student.id
+            WHERE lower(t.subject) LIKE %:subject% AND
+            (creator.username = :username or
+            collaborator.username = :username or
+            student.username = :username)
+    """)
+    List<Test> findAllBySubjectContaining(@Param("subject") String subject,
+                                          @Param("username") String username);
 }
