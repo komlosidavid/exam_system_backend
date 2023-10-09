dedc3f97ac1ea13a864c06af45c352628c7aa1c2
komlosidavid <komlosidavid@mailbox.unideb.hu>
2023-10-09 22:13:01 +0200

    Update TestService.java

--- src/main/java/inf/unideb/hu/exam/system/service/TestService.java
+++ src/main/java/inf/unideb/hu/exam/system/service/TestService.java
@@ -1,14 +1,16 @@
 
 package inf.unideb.hu.exam.system.service;
 
+import inf.unideb.hu.exam.system.models.GetAllTestsFilter;
 import inf.unideb.hu.exam.system.models.Pair;
 import inf.unideb.hu.exam.system.models.Test;
+import inf.unideb.hu.exam.system.models.User;
 import inf.unideb.hu.exam.system.request.CreateTestEntityRequest;
-import inf.unideb.hu.exam.system.request.UpdateTestEntityRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,8 +24,8 @@ public interface TestService {
      * Get all test entities from the database.
      * @return a {@link List} of {@link Test} entities.
      */
-    Page<Test> getAllTests(HttpServletRequest request,
-                           String filter,
+    Page<Test> getAllTests(String username,
+                           GetAllTestsFilter filter,
                            Pageable pageable);
 
     /**
@@ -34,10 +36,17 @@ public interface TestService {
     HttpStatus createTest(CreateTestEntityRequest request);
 
     /**
-     * Function to update a {@link Test} entity.
-     * @param id for identify the {@link Test} entity.
-     * @param request for updating {@link Test} entity.
-     * @return a {@link Pair} class holding the data and the response message.
+     * Function to get all {@link Test} entities by subject property.
+     * @param username of the {@link User}.
+     * @param subject of the {@link Test}.
+     * @return a {@link List} of {@link Test} entities.
      */
-    Pair<Optional<Test>> updateTest(UUID id, UpdateTestEntityRequest request);
+    List<Test> getAllTestsWithSubjectName(String username, String subject);
+
+    /**
+     * Function to get a {@link Test} entity by id.
+     * @param id of the {@link Test}.
+     * @return an {@link Optional} of {@link Test} entity.
+     */
+    Optional<Test> getTestById(UUID id);
 }
